91d79536d7df3defa77e6df6a4eb5178aeb0a73b
jaeheonYoo325 <yoojh0325@gmail.com>
2022-07-04 23:05:39 +0900

    2022.07.04 : 프로젝트 생성.

---
+++ src/main/java/com/springmvc/springmvc2thymeleaf/Springmvc2ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.springmvc2thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc2ThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springmvc2ThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmvc/springmvc2thymeleaf/Springmvc2ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmvc.springmvc2thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc2ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
